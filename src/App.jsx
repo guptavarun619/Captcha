abf79b77a58db4601aa45b67e6eee0a2cc5c3bf2
guptavarun619 <guptavarun619@gmail.com>
2023-01-10 11:26:48 +0530

    🚀 Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <h1 className="py-1 text-3xl text-center font-bold">
+        Capta Assignment : 🚀 Initial setup
+      </h1>
+    </div>
+  );
+}
+
+export default App;
